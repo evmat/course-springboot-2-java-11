1887410c87e73d522eb593aef1031ca584019175
evamat <emattos0114@outlook.com>
2020-07-16 10:45:11 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/exceptions/DatabaseException.java
+++ src/main/java/com/educandoweb/course/services/exceptions/DatabaseException.java
@@ -1,5 +1,10 @@
 package com.educandoweb.course.services.exceptions;
 
-public class DatabaseException {
+public class DatabaseException extends RuntimeException{
 
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
 }
